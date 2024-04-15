f0d9574ef7c0a6f90844592844bc058b20511b9d
Andrew_Polik <polik.24@mail.ru>
2024-04-16 00:37:24 +0300

    В сериализатор модели курса добавлено поле вывода количества уроков. В приложении Users добавлена модель Payment, применены миграции.

--- online_school/serializers/course.py
+++ online_school/serializers/course.py
@@ -4,6 +4,10 @@ from online_school.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, object):
+        return object.lesson_set.count()
 
     class Meta:
         model = Course
--- users/models.py
+++ users/models.py
@@ -20,3 +20,25 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'наличные'),
+        ('card', 'банковский перевод')
+    ]
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='пользователь')
+    date_of_payment = models.DateTimeField(auto_now_add=True, verbose_name='дата платежа')
+    paid_course = models.ForeignKey('online_school.Course', on_delete=models.CASCADE, verbose_name='оплаченный курс')
+    paid_lesson = models.ForeignKey('online_school.Lesson', on_delete=models.CASCADE, verbose_name='оплаченный урок')
+    payment_amount = models.PositiveIntegerField(verbose_name='сумма платежа')
+    payment_method = models.CharField(max_length=100, choices=PAYMENT_METHOD_CHOICES, default='card',
+                                      verbose_name='метод оплаты')
+
+    def __str__(self):
+        return f'Пользователь - {self.user}\nДоступные курсы - {self.paid_course}\nДоступные уроки - {self.paid_lesson}.'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
